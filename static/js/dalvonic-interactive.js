4b97abb2aeaea95d03b46777a68f7af805e34749
Ana-Sophia Brande <Sophie@reshut1-22-6-dhcp.int.colorado.edu>
2016-04-02 03:15:34 -0600

    Javascript Loop

--- static/js/dalvonic-interactive.js
+++ static/js/dalvonic-interactive.js
@@ -1,18 +1,8 @@
-
-var pattern = Trianglify({
-	width: window.innerWidth,
-	height: window.innerHeight,
-	variance: "0.98",
-	seed: '32yy1',
-	x_colors: 'random'});
-
-document.body.appendChild(pattern.canvas())
-// var colorbbrewer = {YlGnBu: {
-// 3: ["#edf8b1","#7fcdbb","#2c7fb8"],
-// 4: ["#ffffcc","#a1dab4","#41b6c4","#225ea8"],
-// 5: ["#ffffcc","#a1dab4","#41b6c4","#2c7fb8","#253494"],
-// 6: ["#ffffcc","#c7e9b4","#7fcdbb","#41b6c4","#2c7fb8","#253494"],
-// 7: ["#ffffcc","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#0c2c84"],
-// 8: ["#ffffd9","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#0c2c84"],
-// 9: ["#ffffd9","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"]
-// }};
+for(var i=0; i<5; i++){
+	hexHeight = (50*i)+1;
+	for(var j=0; j<4; j++){
+		var hexWidth = (50*j)+1;
+		var hex = '<div class="hexagon position" style:"margin-left:'+ hexWidth+'px; margin-top:'+hexHeight+'px"><span></span></div>';
+		$('body').append(hex);
+	}
+}
