0748e05053e5f6e76c6242ec32b50923c66af7f3
Joe Rickard <joe.s.rickard@gmail.com>
2016-04-02 00:53:11 -0600

    Update sentAnalysis.py

--- sentAnalysis.py
+++ sentAnalysis.py
@@ -3,6 +3,7 @@ import tweepy #https://github.com/tweepy/tweepy
 import codecs
 import csv
 import nltk
+import json
 
 class tweet:
   tweet   = ''
@@ -13,6 +14,12 @@ class tweet:
 def sentimentAnalysis(data): #fill topicData with class tweet elements and return.
   topicData = []
   for i in data:
+    new tweet temp
+    temp.tweet = 
+    temp.tweetID = 
+    temp.x = #nltk output
+    temp.y = 0
+    topicData.append(i)
     
     
   
