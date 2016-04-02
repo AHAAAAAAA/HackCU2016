db260fe76606fb4d11606ab04f4dbbaefb55a0d5
Ana-Sophia Brande <Sophie@reshut1-22-6-dhcp.int.colorado.edu>
2016-04-02 16:19:39 -0600

    Added Javascript and main changes

--- static/js/dalvonic-interactive.js
+++ static/js/dalvonic-interactive.js
@@ -1,8 +1,8 @@
-for(var i=0; i<5; i++){
-	hexHeight = (50*i)+1;
-	for(var j=0; j<4; j++){
-		var hexWidth = (50*j)+1;
-		var hex = '<div class="hexagon position" style:"margin-left:'+ hexWidth+'px; margin-top:'+hexHeight+'px"><span></span></div>';
-		$('body').append(hex);
-	}
-}
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
