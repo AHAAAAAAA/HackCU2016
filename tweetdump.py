d3e502fae638ddb556ddc1583254a80f5dae5432
Ahmed Almutawa <ahmed.almutawa@colorado.edu>
2016-04-03 02:22:45 -0600

    tweetdump fun

--- tweetdump.py
+++ tweetdump.py
@@ -20,7 +20,15 @@ def get_all_tweets(topic):
 	alltweets = []	
 	#ahmed was here
 	#make initial request for most recent tweets (200 is the maximum allowed count)
-	new_tweets = api.search(q=topic, rpp=200)
+	page_count = 0  
+	for tweets in tweepy.Cursor(api.search, q=topic, count=100,
+                result_type="recent", include_entities=True).pages():  
+		page_count += 1  
+		# print just the first tweet out of every page of 100 tweets  
+		print tweets[0].text.encode('utf-8')  
+		if page_count >= 200:  
+			break
+	# new_tweets = tweepy.Cursor(api.search(q=topic)).items(200)
 	
 	#save most recent tweets
 	alltweets.extend(new_tweets)
@@ -28,16 +36,16 @@ def get_all_tweets(topic):
 	#save the id of the oldest tweet less one
 	oldest = alltweets[-1].id - 1
 	
-	#keep grabbing tweets until there are no tweets left to grab
-	while len(new_tweets) > 0:
-		#all subsiquent requests use the max_id param to prevent duplicates
-		new_tweets = api.search(q=topic, rpp=200)	
+	# #keep grabbing tweets until there are no tweets left to grab
+	# while len(new_tweets) > 0:
+	# 	#all subsiquent requests use the max_id param to prevent duplicates
+	# 	new_tweets = api.search(q=topic, rpp=200)	
 		
-		#save most recent tweets
-		alltweets.extend(new_tweets)
+	# 	#save most recent tweets
+	# 	alltweets.extend(new_tweets)
 		
-		#update the id of the oldest tweet less one
-		oldest = alltweets[-1].id - 1
+	# 	#update the id of the oldest tweet less one
+	# 	oldest = alltweets[-1].id - 1
 	
 	#transform the tweepy tweets into a 2D array that will populate the csv	
 	outtweets = [[tweet.text.encode("utf-8"), tweet.id_str, tweet.author._json['screen_name'], tweet.created_at,] for tweet in alltweets]
