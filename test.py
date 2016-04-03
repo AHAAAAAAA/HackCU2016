288cd2dbecb9ba4fc3debd3e483bc645de1cb64a
Joseph Rickard <joey@reshut1-24-7-dhcp.int.colorado.edu>
2016-04-02 19:53:14 -0600

    first attempt at full data processing

--- test.py
+++ test.py
@@ -1,11 +1,10 @@
 from tweetdump import *
 from senti_classifier import senti_classifier
 import nltk
-nltk.download('movie_reviews')
-nltk.download('sentiwordnet')
-nltk.download('wordnet')
-nltk.download('twitter_samples')
-nltk.download('punkt')
-sentences = ['Fuck this', 'I love you']
-pos_score, neg_score = senti_classifier.polarity_scores(sentences)
-print (pos_score, neg_score)
+#call to tweetdump with topic hashtag input
+opinions = get_all_tweets("fidelity")
+#pass tweetdump output to sentAnalysis
+opinions = preprocessTweets(opinions)
+opinions = sentimentAnalysis(opinions)
+print opinions
+#Pass complete tweet topic info to front end for display.
